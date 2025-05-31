3d441515a64a851624e5166c84e74a77c3555b22
JnrPtt <113989334+JnrPtt@users.noreply.github.com.>
2025-05-31 02:35:57 +0200

    Nuevas funcionalidades

--- src/main/java/org/example/CLIManager.java
+++ src/main/java/org/example/CLIManager.java
@@ -1,10 +1,23 @@
 package org.example;
 
 public class CLIManager {
-//    1. Agregar contacto
-//    2. Ver contactos
-//    3. Buscar contacto
-//    4. Editar contacto
-//    5. Eliminar contacto
-//    6. Salir
+    //    1. Agregar contacto
+    //    2. Ver contactos
+    //    3. Buscar contacto
+    //    4. Editar contacto
+    //    5. Eliminar contacto
+    //    6. Salir
+
+    public void start() {
+        while(true) {
+            mostrarMenu();
+        }
+    }
+
+    public void mostrarMenu() {
+
+    }
+
+
+
 }
--- src/main/java/org/example/ContactManager.java
+++ src/main/java/org/example/ContactManager.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import javax.xml.crypto.Data;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +11,7 @@ public class ContactManager {
         String sql = "INSERT INTO contactos (nombre, telefono, email) VALUES (?, ?, ?)";
 
         try (Connection conn = DatabaseManager.connect();
-            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
             pstmt.setString(1, contact.getNombre());
             pstmt.setString(2, contact.getTelefono());
@@ -28,8 +29,8 @@ public class ContactManager {
         String sql = "SELECT * FROM contactos";
 
         try (Connection conn = DatabaseManager.connect();
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(sql)) {
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
 
             while(rs.next()) {
                 Contact contact = new Contact(
@@ -45,4 +46,72 @@ public class ContactManager {
         }
         return contacts;
     }
+
+    public List<Contact> searchContact(String search) {
+        List<Contact> contacts = new ArrayList<>();
+        String sql = "SELECT * FROM contactos WHERE nombre LIKE ?";
+
+        try (Connection conn = DatabaseManager.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery();) {
+
+            pstmt.setString(1, search);
+
+            while (rs.next()) {
+                Contact contact = new Contact(
+                        rs.getInt("id"),
+                        rs.getString("nombre"),
+                        rs.getString("telefono"),
+                        rs.getString("email")
+                );
+                contacts.add(contact);
+            }
+        }   catch (SQLException e) {
+            System.err.println("Error al buscar contacto" + e.getMessage());
+        }
+        return contacts;
+    }
+
+    public void updateContact(Contact contact) {
+        String sql = "UPDATE contactos SET nombre = ?, telefono = ?, email = ? WHERE id = ?";
+
+        try (Connection conn = DatabaseManager.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1,contact.getNombre());
+            pstmt.setString(2, contact.getTelefono());
+            pstmt.setString(3, contact.getEmail());
+            pstmt.setInt(4, contact.getId());
+
+            int affectedRows = pstmt.executeUpdate();
+            if (affectedRows > 0) {
+                System.out.println("Contacto actualizado");
+            }   else {
+                System.err.println("Contacto no encontrado");
+            }
+        }   catch (SQLException e) {
+            System.err.println("Error al actualizar contacto" + e.getMessage());
+        }
+    }
+    
+    public void deleteContact(int contactId) {
+        String sql = "DELETE FROM contactos WHERE id = ?";
+
+        try (Connection conn = DatabaseManager.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, contactId);
+            int affectedRows = pstmt.executeUpdate();
+
+            if (affectedRows > 0) {
+                System.out.println("Contacto eliminado");
+            }   else {
+                System.err.println("Contacto no encontrado");
+            }
+
+        }   catch (SQLException e) {
+            System.err.println("Error al eliminar contacto" + e.getMessage());
+        }
+    }
+
 }
